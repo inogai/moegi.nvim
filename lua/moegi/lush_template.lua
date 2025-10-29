780f5f880edfaa52970659568a591c599b0ce089
inogai <inogai.sign@gmail.com>
2025-10-29 14:42:57 +0800

    fix: constant_gui no effect

--- lua/moegi/lush_template.lua
+++ lua/moegi/lush_template.lua
@@ -129,7 +129,7 @@ local theme = lush(function(injected_functions)
 
     -- Comment        { }, -- Any comment
 
-    Constant       { fg = C.pale, gui = "bold" }, -- (*) Any constant
+    Constant       { fg = C.pale, gui = c.constant_gui }, -- (*) Any constant
     String         { fg = C.green, gui = c.string_gui }, --   A string constant: "this is a string"
     Character      { fg = C.green, gui = c.string_gui }, --   A character constant: 'c', '\n'
     Number         { fg = C.gold, gui = c.keyword_gui }, --   A number constant: 234, 0xff
@@ -221,22 +221,7 @@ local theme = lush(function(injected_functions)
     -- DiagnosticUnnecessary       { } , -- Used for unnecessary or unused code.
 
     -- Tree-Sitter syntax groups.
-    --
-    -- See :h treesitter-highlight-groups, some groups may not be listed,
-    -- submit a PR fix to lush-template!
-    --
-    -- Tree-Sitter groups are defined with an "@" symbol, which must be
-    -- specially handled to be valid lua code, we do this via the special
-    -- sym function. The following are all valid ways to call the sym function,
-    -- for more details see https://www.lua.org/pil/5.html
-    --
-    -- sym("@text.literal")
-    -- sym('@text.literal')
-    -- sym"@text.literal"
-    -- sym'@text.literal'
-    --
-    -- For more information see https://github.com/rktjmp/lush.nvim/issues/109
-
+    -- ``:h treesitter-highlight-groups``
     -- sym"@text.literal"      { }, -- Comment
     -- sym"@text.reference"    { }, -- Identifier
     -- sym"@text.title"        { }, -- Title
