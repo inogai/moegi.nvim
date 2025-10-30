b3dd2943ca171cc48d9bf95fda94a1d8e8bbd6e0
inogai <inogai.sign@gmail.com>
2025-10-30 17:25:15 +0800

    fix(config): flavour should be optional

--- lua/moegi/config.lua
+++ lua/moegi/config.lua
@@ -7,8 +7,8 @@
 ---@field diagnostic_underline_gui string the gui opts for diagnostic underlines
 
 ---@class moegi.Config
----@field flavour_light string  the flavour to use in light mode
----@field flavour_dark string   the flavour to use in dark mode
+---@field flavour_light? string  the flavour to use in light mode
+---@field flavour_dark? string   the flavour to use in dark mode
 ---@field constant_gui? string   the gui opts for constant variable
 ---@field string_gui? string     the gui opts for string & char literals
 ---@field keyword_gui? string    the gui opts for keywords and numeric / boolean literals
