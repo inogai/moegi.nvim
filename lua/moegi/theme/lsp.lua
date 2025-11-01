f137be7a46ef032fa0757c5a7b8f393326702040
inogai <inogai.sign@gmail.com>
2025-11-01 11:30:55 +0800

    fix(hl): LspReferenceText

--- lua/moegi/theme/lsp.lua
+++ lua/moegi/theme/lsp.lua
@@ -4,6 +4,7 @@ return {
   -- See :h lsp-highlight and :h diagnostic-highlights
   LspInlayHint                = { fg = U.vary_color({ dark = C.muted, light = C.green }), bg = U.vary_color({ dark = C.overlay1, light = C.overlay2 }) },
   LspCodeLens                 = { fg = C.muted },
+  LspReferenceText            = { bg = C.overlay2 },
   DiagnosticError             = { fg = C.red },
   DiagnosticWarn              = { fg = C.yellow },
   DiagnosticInfo              = { fg = C.blue },
