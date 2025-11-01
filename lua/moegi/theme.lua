efdcbda315d7d40803df6d7ea445107b79e82088
inogai <inogai.sign@gmail.com>
2025-11-01 11:19:04 +0800

    ref: improve hlgroup def

--- lua/moegi/theme.lua
+++ lua/moegi/theme.lua
@@ -15,12 +15,17 @@ vim.g.colors_name = "moegi"
 
 local highlights = {}
 
----@class moegi.hl
+---@class moegi.hl.Link
+---@field link string
+
+---@class moegi.hl.Generic
+---@field [1]? string
 ---@field fg? string
 ---@field bg? string
 ---@field sp? string
----@field link? string
 ---@field gui? string[]
+
+---@alias moegi.hl moegi.hl.Link|moegi.hl.Generic
 
 function highlights_add(spec)
   package.loaded[spec] = nil
@@ -43,14 +48,36 @@ for integration, enabled in pairs(O.integrations or {}) do
   end
 end
 
----@type moegi.hl
+--- Resolve a highlight group by name
+---@param name string
+---@return moegi.hl.Generic
+local function resolve_hl(name)
+  local hl = highlights[name]
+
+  if hl.link then
+    return resolve_hl(hl.link)
+  end
+
+  return hl
+end
+
+---@param hl moegi.hl
 local function to_nvim_hl(hl)
   local nvim_hl = {}
+
+  if hl.link then
+    nvim_hl.link = hl.link
+    return nvim_hl
+  end
+
+  if hl[1] then
+    local base = resolve_hl(hl[1])
+    hl = vim.deepcopy(base)
+  end
 
   nvim_hl.fg = hl.fg
   nvim_hl.bg = hl.bg
   nvim_hl.sp = hl.sp
-  nvim_hl.link = hl.link -- TODO: maybe consider a augment-based link?
 
   for k, v in pairs(hl.gui or {}) do
     nvim_hl[v] = true
